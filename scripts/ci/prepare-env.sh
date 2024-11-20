0caa55d43cadf53c8953ef312c50cd5c31f85687
Brian Luk <brian6.dev@gmail.com>
2024-11-19 22:45:05 -0800

    update prepare-env.sh

--- scripts/ci/prepare-env.sh
+++ scripts/ci/prepare-env.sh
@@ -2,15 +2,15 @@
 
 sscd init test --chain-id testchain
 cp ./scripts/ci/config/client.toml ~/.ssc/config/
-sscd keys add alice
-sscd keys add bob
+sscd keys add alice 2>&1 > /dev/null
+sscd keys add bob 2>&1 > /dev/null
 sscd add-genesis-account "$(sscd keys show alice -a)" 100000000000000000000000000utsaga,100000000stake
 sscd add-genesis-account "$(sscd keys show bob -a)" 100000000000000000000000000utsaga,100000000stake
 jq '.app_state["chainlet"]["params"]["chainletStackProtections"]=true' ~/.ssc/config/genesis.json > ~/.ssc/config/tmp_genesis.json && mv ~/.ssc/config/tmp_genesis.json ~/.ssc/config/genesis.json
 jq '.app_state["chainlet"]["params"]["nEpochDeposit"]="30"' ~/.ssc/config/genesis.json > ~/.ssc/config/tmp_genesis.json && mv ~/.ssc/config/tmp_genesis.json ~/.ssc/config/genesis.json
 jq '.app_state["acl"]["params"]["enable"]=true' > ~/.ssc/config/tmp_genesis.json ~/.ssc/config/genesis.json && mv ~/.ssc/config/tmp_genesis.json ~/.ssc/config/genesis.json
-jq ".app_state[\"acl\"][\"allowed\"]=[{\"format\":1,\"value\":\"$(sscd keys show bob -a)\"}]" ~/.ssc/config/genesis.json > ~/.ssc/config/tmp_genesis.json && mv ~/.ssc/config/tmp_genesis.json ~/.ssc/config/genesis.json
-sscd gentx alice 100000000stake --chain-id testchain
-sscd collect-gentxs
+jq ".app_state[\"acl\"][\"allowed\"]=[\"$(sscd keys show bob -a)\"]" ~/.ssc/config/genesis.json > ~/.ssc/config/tmp_genesis.json && mv ~/.ssc/config/tmp_genesis.json ~/.ssc/config/genesis.json
+sscd genesis gentx alice 100000000stake --chain-id testchain
+sscd genesis collect-gentxs
 sscd start &
 sleep 10

125195b1dbaca1eee858951ac87e47ee6c8489d7
Roman Kollár <10644651+rkollar@users.noreply.github.com>
2025-12-19 03:00:58 +0100

    Add v2 upgrade handler (#126)

--- app/app.go
+++ app/app.go
@@ -157,6 +157,7 @@ import (
 	upgrade02 "github.com/sagaxyz/ssc/app/upgrades/0.2"
 	upgrade03 "github.com/sagaxyz/ssc/app/upgrades/0.3"
 	upgrade1 "github.com/sagaxyz/ssc/app/upgrades/1.0"
+	upgrade2 "github.com/sagaxyz/ssc/app/upgrades/2"
 
 	// this line is used by starport scaffolding # stargate/app/moduleImport
 
@@ -1264,6 +1265,7 @@ func (app *App) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(upgrade02.Name, upgrade02.UpgradeHandler(app.mm, app.configurator, app.ParamsKeeper, &app.ConsensusParamsKeeper, baseAppLegacySS))
 	app.UpgradeKeeper.SetUpgradeHandler(upgrade03.Name, upgrade03.UpgradeHandler(app.mm, app.configurator))
 	app.UpgradeKeeper.SetUpgradeHandler(upgrade1.Name, upgrade1.UpgradeHandler(app.mm, app.configurator, app.AccountKeeper, app.BankKeeper, app.ProviderKeeper, app.DacKeeper, *app.ChainletKeeper, app.BillingKeeper))
+	app.UpgradeKeeper.SetUpgradeHandler(upgrade2.Name, upgrade2.UpgradeHandler(app.mm, app.configurator))
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
@@ -1295,6 +1297,8 @@ func (app *App) RegisterUpgradeHandlers() {
 				peerstypes.StoreKey,
 			},
 		}
+	case upgrade2.Name:
+		storeUpgrades = &storetypes.StoreUpgrades{}
 	default:
 	}
 	if storeUpgrades != nil {
